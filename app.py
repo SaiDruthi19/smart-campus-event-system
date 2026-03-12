a67f2aba46c4c83db2cdfb3e9eca4707976fa47a
Sai Druthi <satraboinasaidruthi@gmail.com>
2026-03-13 01:33:40 +0530

    Use Render PORT environment variable

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -133,4 +134,5 @@ def analytics():
 
 # Run server
 if __name__ == "__main__":
-    app.run(debug=True)
+        port = int(os.environ.get("PORT", 5000))  # Use Render’s port or 5000 locally
+        app.run(host="0.0.0.0", port=port)   
